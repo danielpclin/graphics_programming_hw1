6bd3e81b95b458aa5d2bcdc2f0d81f71133ce924
danielpclin <daniel.pclin@gmail.com>
2022-10-25 16:21:44 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "Common.h"
 
-
 Shader *materialShader;
 Shader *textureShader;
 Model* capsule;
@@ -12,6 +11,11 @@ Texture* woodTexture;
 Scene *scene;
 glm::mat4 projection_matrix(1.0f);
 float model_rotation = 0.0f;
+
+//imgui state
+bool show_demo_window = true;
+bool show_another_window = false;
+ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 void init()
 {
@@ -34,7 +38,7 @@ void init()
     sphere = new Model("model/Sphere.obj");
 
     // Load textures
-    woodTexture = new Texture("texture/wood.jpg");
+    woodTexture = new Texture("texture/container.jpg");
 
     // setup material shader
     // setup light uniform
@@ -121,7 +125,53 @@ void init()
     });
 }
 
-// GLUT callback. Called to draw the scene.
+void prepare_imgui()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+        ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+
+    // 3. Show another simple window.
+    if (show_another_window)
+    {
+        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me"))
+            show_another_window = false;
+        ImGui::End();
+    }
+
+    // Render
+    ImGui::Render();
+
+}
 void draw()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -140,8 +190,6 @@ void draw()
     scene->draw();
 
 }
-
-
 // whenever the mouse scroll wheel scrolls, this callback is called
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
@@ -172,6 +220,8 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 static void GLAPIENTRY debugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam )
 {
+    if (severity == GL_DEBUG_SEVERITY_NOTIFICATION)
+        return;
     fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
              ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
              type, severity, message);
@@ -189,7 +239,7 @@ int main(int argc, char *argv[])
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    window = glfwCreateWindow(640, 480, "Graphics programming assignment 1", NULL, NULL);
+    window = glfwCreateWindow(1000, 800, "Graphics programming assignment 1", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -210,6 +260,7 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+
     glfwSwapInterval(1);
 
     glEnable(GL_DEBUG_OUTPUT);
@@ -217,21 +268,59 @@ int main(int argc, char *argv[])
         glDebugMessageCallback(debugMessageCallback, nullptr);
 
     printGLContextInfo();
+
+    // setup viewport
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+
+    glViewport(0, 0, width, height);
+    projection_matrix = glm::perspective(glm::radians(45.0f), width / (float) height, 0.1f, 100.0f);
+
     init();
+
+    // setup imgui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+    //io.ConfigViewportsNoAutoMerge = true;
+    //io.ConfigViewportsNoTaskBarIcon = true;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
 
     while (!glfwWindowShouldClose(window))
     {
-        float viewportAspect;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
+        glfwPollEvents();
 
-        glViewport(0, 0, width, height);
-        projection_matrix = glm::perspective(glm::radians(45.0f), width / (float) height, 0.1f, 100.0f);
-
+        prepare_imgui();
         draw();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
 
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
